3a33345b2b96be7083a53b437fbc0d5061a31efb
jbethke6 <justin.bethke.ftr@web.de>
2026-03-25 23:51:26 +0100

    Update logo image in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "@/assets/logo.jpeg";
+import logo from "@/assets/7c8eae15-1470-48a0-8ceb-1ddf732aef62.png";
 
 export function Footer() {
   return (
